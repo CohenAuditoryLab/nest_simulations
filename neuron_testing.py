c71207b8d561807891e19e3902bb7d529f84232c
marsalad <marsalad@sas.upenn.edu>
2017-05-25 13:59:00 -0400

    3000 neurons

--- neuron_testing.py
+++ neuron_testing.py
@@ -2,12 +2,31 @@ import nest
 import matplotlib.pyplot as plt
 import nest.voltage_trace
 
-neuron_mod = 'iaf_psc_exp'
-neuron_param = {
-	'C_m'     : 1.0,  # membrane capacitance (pF)
-	'E_L'     : 0.0,  # resting membrane potential (mV)
-	'V_reset' : 0.0,  # reset membrane potential after spiking (mV)
-	'V_th'    : 20.0, # spike threshold (mV) SUBJECT TO CHANGE
-	'tau_m'   : 20.0  # membrane time constant (ms)
-}
-nest.ResetKernel()
+def new_neurons(n):
+	neuron_mod = 'iaf_psc_alpha'
+	neuron_param = {
+		'E_L'     : -70.0, # resting membrane potential (mV)
+		'V_reset' : -70.0, # reset membrane potential after spiking (mV)
+		'V_th'    : -55.0, # spike threshold (mV)
+		'tau_m'   : 10.0   # membrane time constant (ms)
+	}
+	return nest.Create(neuron_mod, n, neuron_param)
+
+# frequency is some integer 1000 < n <= 4000
+def stimulate(frequency, time):
+	nest.SetStatus([stimuli[frequency-1001]], {'rate': 2000.0})
+	nest.Simulate(time)
+	nest.SetStatus([stimuli[frequency-1001]], {'rate': 0.0})
+
+
+stimuli = nest.Create('poisson_generator', 3000)
+neurons = new_neurons(3000)
+voltmeter = nest.Create('voltmeter')
+nest.Connect(stimuli, neurons, 'one_to_one', {'weight': 50.0})
+nest.Connect(voltmeter, neurons)
+
+for i in range(1001,4001):
+	stimulate(i,25)
+
+nest.voltage_trace.from_device(voltmeter)
+plt.show()

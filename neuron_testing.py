9b80ee4219829ec2ca356af247fa1bc05aad43f4
marsalad <marsalad@sas.upenn.edu>
2017-05-25 12:02:41 -0400

    removed unnecessary plasticity settings

--- neuron_testing.py
+++ neuron_testing.py
@@ -2,6 +2,7 @@ import nest
 import matplotlib.pyplot as plt
 import nest.voltage_trace
 
+neuron_mod = 'iaf_psc_exp'
 neuron_param = {
 	'C_m'     : 1.0,  # membrane capacitance (pF)
 	'E_L'     : 0.0,  # resting membrane potential (mV)
@@ -9,15 +10,4 @@ neuron_param = {
 	'V_th'    : 20.0, # spike threshold (mV) SUBJECT TO CHANGE
 	'tau_m'   : 20.0  # membrane time constant (ms)
 }
-syn_static_param = {
-	'delay'   : 1.5 # time delay (ms)
-}
-syn_dep_param = {
-	'U'       : 0.5, # facilitation parameter U
-	'delay'   : 1.5, # time delay (ms)
-	'tau_rec' : 50.0 # facilitation time (ms)
-}
-nest.SetDefaults('iaf_psc_exp', neuron_param) # LIF neuron w/ exponential PSCs
-nest.SetDefaults('static_synapse', syn_static_param) # standard static synapse
-nest.SetDefaults('tsodyks2_synapse', syn_dep_param) # Tsodyks-Markram synapse
 nest.ResetKernel()

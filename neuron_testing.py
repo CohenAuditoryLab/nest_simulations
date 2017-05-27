c0969129cbc534c48e83cd55156cc5019835d858
marsalad <marsalad@sas.upenn.edu>
2017-05-27 16:57:55 -0400

    connected poisson to 10 neurons

--- neuron_testing.py
+++ neuron_testing.py
@@ -4,10 +4,35 @@ import nest.voltage_trace
 
 neuron_mod = 'iaf_psc_exp'
 neuron_param = {
-	'C_m'     : 1.0,  # membrane capacitance (pF)
-	'E_L'     : 0.0,  # resting membrane potential (mV)
-	'V_reset' : 0.0,  # reset membrane potential after spiking (mV)
-	'V_th'    : 20.0, # spike threshold (mV) SUBJECT TO CHANGE
-	'tau_m'   : 20.0  # membrane time constant (ms)
+	'E_L'     : -70.0, # resting membrane potential (mV)
+	'V_reset' : -70.0, # reset membrane potential after spiking (mV)
+	'V_th'    : -55.0, # spike threshold (mV)
+	'tau_m'   : 10.0   # membrane time constant (ms)
+}
+stim_param = {
+	'rate': 2000.0 # firing rate (Hz)
 }
 nest.ResetKernel()
+
+# Generate stimulus, send identical spike trains to target neurons
+stim_gen = nest.Create('poisson_generator', params=stim_param)
+stim = nest.Create('parrot_neuron')
+nest.Connect(stim_gen,stim)
+
+# Create and connect neurons
+neurons = nest.Create(neuron_mod, 10, neuron_param)
+voltmeter = nest.Create('voltmeter')
+spk_det = nest.Create('spike_detector')
+nest.Connect(stim, neurons, syn_spec={'weight':100.0})
+nest.Connect(voltmeter, neurons)
+nest.Connect(neurons, spk_det)
+
+nest.Simulate(500)
+
+# Plot membrane potential and spike trains
+plt.subplot(211)
+nest.voltage_trace.from_device(voltmeter)
+plt.subplot(212)
+events = nest.GetStatus(spk_det)[0]['events']
+plt.plot(events['times'],events['senders'], 'o')
+plt.show()

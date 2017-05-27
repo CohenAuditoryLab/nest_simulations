24f73864a88a90c8b518ee94e24ac0de7bdc7bba
marsalad <marsalad@sas.upenn.edu>
2017-05-27 18:00:11 -0400

    working tonotopic map with downstream neurons

--- neuron_testing.py
+++ neuron_testing.py
@@ -2,6 +2,12 @@ import nest
 import matplotlib.pyplot as plt
 import nest.voltage_trace
 
+###########################################
+####   PARAMETERS   #######################
+###########################################
+
+nest.ResetKernel()
+
 neuron_mod = 'iaf_psc_exp'
 neuron_param = {
 	'E_L'     : -70.0, # resting membrane potential (mV)
@@ -12,26 +18,45 @@ neuron_param = {
 stim_param = {
 	'rate': 2000.0 # firing rate (Hz)
 }
-nest.ResetKernel()
+freq_num = 4 # number of stimuli frequencies
+neuron_num = 6 # number of downstream neurons
+
+###########################################
+####   CONNECTIONS   ######################
+###########################################
 
 # Generate stimulus, send identical spike trains to target neurons
-stim_gen = nest.Create('poisson_generator', params=stim_param)
 stim = nest.Create('parrot_neuron')
-nest.Connect(stim_gen,stim)
+nest.Connect(nest.Create('poisson_generator', params=stim_param),stim)
 
-# Create and connect neurons
-neurons = nest.Create(neuron_mod, 10, neuron_param)
-voltmeter = nest.Create('voltmeter')
+# Create tonotopic map of frequencies
+tono_map = nest.Create(neuron_mod, freq_num, neuron_param)
+neurons = nest.Create(neuron_mod, neuron_num, neuron_param)
+
+# Connect tonotopic map to downstream neurons
+for i in range(len(tono_map)):
+	nest.Connect(tono_map[i:i+1],neurons[i:i+3], syn_spec={'weight':1000.0})
+
+# Create and connect devices
+volt_mtr = nest.Create('voltmeter')
 spk_det = nest.Create('spike_detector')
-nest.Connect(stim, neurons, syn_spec={'weight':100.0})
-nest.Connect(voltmeter, neurons)
+nest.Connect(volt_mtr, neurons)
 nest.Connect(neurons, spk_det)
+nest.Connect(stim, tono_map, syn_spec={'weight': 100.0})
+
+###########################################
+####   SIMULATION   #######################
+###########################################
 
 nest.Simulate(500)
 
+###########################################
+####   PLOTTING   #########################
+###########################################
+
 # Plot membrane potential and spike trains
 plt.subplot(211)
-nest.voltage_trace.from_device(voltmeter)
+nest.voltage_trace.from_device(volt_mtr)
 plt.subplot(212)
 events = nest.GetStatus(spk_det)[0]['events']
 plt.plot(events['times'],events['senders'], 'o')

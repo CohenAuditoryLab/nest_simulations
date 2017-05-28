d8d45fceba8107ca4bb9dee3c9cd181c63730df1
marsalad <marsalad@sas.upenn.edu>
2017-05-27 21:22:10 -0400

    added firing rate calculations

--- neuron_testing.py
+++ neuron_testing.py
@@ -18,7 +18,7 @@ stim_rate = 2000.0 # stimulus rate (Hz)
 stim_weight = 500.0 # strength of connection from stimulus to tonotopic map
 freq_num = 9 # number of auditory frequencies
 neuron_num = 11 # number of downstream neurons
-sim_time = 500 # duration of simulation (ms)
+sim_time = 500.0 # duration of simulation (ms)
 
 nest.ResetKernel() # reset NEST
 np.random.seed(20) # set seed for reproducability
@@ -42,25 +42,33 @@ neurons = nest.Create(neuron_mod, neuron_num, neuron_param)
 for i in range(len(tono_map)):
 	nest.Connect(tono_map[i:i+1],neurons[i:i+3], syn_spec={'weight':1000.0})
 
-# Create and connect devices
-volt_mtr = nest.Create('voltmeter')
+# Connect spike detectors
 spk_det = nest.Create('spike_detector')
-nest.Connect(volt_mtr, neurons)
 nest.Connect(neurons, spk_det)
 
 ###########################################
-####   PLOTTING   #########################
+####   RESULTS   ##########################
 ###########################################
 
+# Store data on firing rates for each frequency simulation
+firing_rates = []
+
 # Plot membrane potential and spike trains
-def plotSim(frequency):
+def plot_sim(frequency, events):
 	plt.subplot(331+frequency)
 	plt.title("Spike Trains at %s Hz" % (1000*(frequency+1)/2))
 	plt.xlabel('time (ms)')
 	plt.ylabel('neuron ID')
-	events = nest.GetStatus(spk_det)[0]['events']
 	plt.gca().set_ylim(0, neuron_num-1)
 	plt.plot(events['times'], events['senders']-neuron_num-1, 'o')
+
+def store_firing_rates(frequency, events):
+	sender_fires = [0] * neuron_num
+	for neuron_id in events['senders']:
+		sender_fires[neuron_id-neuron_num-1] += 1
+	for i in range(neuron_num):
+		sender_fires[i] = sender_fires[i]/sim_time
+	firing_rates.append((frequency, sender_fires))
 
 ###########################################
 ####   SIMULATIONS   ######################
@@ -68,10 +76,13 @@ def plotSim(frequency):
 
 for freq in range(freq_num):
 	nest.ResetNetwork()
+	nest.SetKernelStatus({'time': 0.0})
 	nest.SetStatus(conns[freq:freq+1],{'weight': stim_weight})
 	nest.Simulate(sim_time)
 	nest.SetStatus(conns[freq:freq+1],{'weight': 0.0})
-	plotSim(freq)
+	evs = nest.GetStatus(spk_det)[0]['events']
+	plot_sim(freq, evs)
+	store_firing_rates(freq, evs)
 
 ###########################################
 ####   SHOW RESULTS   #####################
@@ -80,3 +91,4 @@ for freq in range(freq_num):
 plt.subplots_adjust(wspace=0.3,hspace=0.6)
 plt.gcf().set_size_inches(12,10,forward=True)
 plt.show()
+print firing_rates

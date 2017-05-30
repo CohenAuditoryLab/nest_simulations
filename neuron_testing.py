0199775ce586a65ea52fd41f2993f5091469acba
marsalad <marsalad@sas.upenn.edu>
2017-05-30 13:57:36 -0400

    added tuning radius capabilities and changed tonotopic map from parrot_neuron to poisson_generator

--- neuron_testing.py
+++ neuron_testing.py
@@ -15,8 +15,8 @@ neuron_param = {
 	'V_th'    : -55.0, # spike threshold (mV)
 	'tau_m'   :  10.0  # membrane time constant (ms)
 }
-stim_rate = 2000.0 # stimulus rate (Hz)
-stim_weight = 500.0 # strength of connection from stimulus to tonotopic map
+base_stim_rate = 2500.0 # stimulus rate (Hz)
+tuning_rad = 2.0 # broadness of tuning curve
 freq_num = 9 # number of auditory frequencies
 neuron_num = 11 # number of downstream neurons
 sim_time = 500.0 # duration of simulation (ms)
@@ -25,54 +25,25 @@ sim_time = 500.0 # duration of simulation (ms)
 def freq_convert(x):
 	return 1000*(x+1)/2
 
+firing_rates = [[] for i in range(neuron_num)] # Initialize firing rates array
+
 nest.ResetKernel() # reset NEST
-np.random.seed(20) # set seed for reproducability
 
 ###########################################
 ####   NETWORK SETUP   ####################
 ###########################################
 
-# Generate stimulus and send identical spike trains through parrot neuron
-stim_gen = nest.Create('poisson_generator', params={'rate': stim_rate})
-stim = nest.Create('parrot_neuron')
-nest.Connect(stim_gen, stim)
-
-# Connect stimulus to tonotopic map, all synapses initially deactivated
-tono_map = nest.Create(neuron_mod, freq_num, neuron_param)
-nest.Connect(stim, tono_map, syn_spec={'weight': 0.0})
-conns = nest.GetConnections(stim)
+# Tonotopic map of all simulated frequencies
+tono_map = nest.Create('poisson_generator',freq_num)
 
 # Connect tonotopic map to downstream neurons
 neurons = nest.Create(neuron_mod, neuron_num, neuron_param)
 for i in range(len(tono_map)):
-	nest.Connect(tono_map[i:i+1],neurons[i:i+3], syn_spec={'weight':1000.0})
+	nest.Connect(tono_map[i:i+1],neurons[i:i+3], syn_spec={'weight':100.0})
 
 # Connect spike detectors
 spk_det = nest.Create('spike_detector')
 nest.Connect(neurons, spk_det)
-
-###########################################
-####   GET RESULTS   ######################
-###########################################
-
-# Store data on firing rates for each frequency simulation
-firing_rates = [[] for i in range(neuron_num)]
-
-# Plot membrane potential and spike trains
-def plot_sim(frequency, events):
-	plt.subplot(331+frequency)
-	plt.title("Spike Trains at %s Hz" % freq_convert(frequency))
-	plt.xlabel('time (ms)')
-	plt.ylabel('neuron ID')
-	plt.gca().set_ylim(0, neuron_num-1)
-	plt.plot(events['times'], events['senders']-neuron_num-1, 'o')
-
-def store_firing_rates(frequency, events):
-	sender_fires = [0] * neuron_num
-	for neuron_id in events['senders']:
-		sender_fires[neuron_id-neuron_num-1] += 1
-	for i in range(neuron_num):
-		firing_rates[i].append(sender_fires[i]/sim_time)
 
 ###########################################
 ####   SIMULATE   #########################
@@ -81,12 +52,34 @@ def store_firing_rates(frequency, events):
 for freq in range(freq_num):
 	nest.ResetNetwork()
 	nest.SetKernelStatus({'time': 0.0})
-	nest.SetStatus(conns[freq:freq+1],{'weight': stim_weight})
+	
+	# Set rate for each tonotopic neuron based on frequency of stimulus
+	for neuron in tono_map:
+		diff = abs(freq-neuron)
+		if diff < tuning_rad:
+			rate_factor = (tuning_rad-diff)/tuning_rad
+			nest.SetStatus([neuron],{'rate': rate_factor*base_stim_rate})
+		else:
+			nest.SetStatus([neuron],{'rate': 0.0})
+	
+	# Simulate and record event data from spike detector
 	nest.Simulate(sim_time)
-	nest.SetStatus(conns[freq:freq+1],{'weight': 0.0})
 	evs = nest.GetStatus(spk_det)[0]['events']
-	plot_sim(freq, evs)
-	store_firing_rates(freq, evs)
+	
+	# Plot spike trains
+	plt.subplot(331+freq)
+	plt.title("Spike Trains at %s Hz" % freq_convert(freq))
+	plt.xlabel('time (ms)')
+	plt.ylabel('neuron ID')
+	plt.gca().set_ylim(0, neuron_num-1)
+	plt.plot(evs['times'], evs['senders']-neuron_num-1, 'o')
+
+	# Store data on firing rates
+	sender_fires = [0] * neuron_num
+	for neuron_id in evs['senders']:
+		sender_fires[neuron_id-neuron_num-1] += 1
+	for i in range(neuron_num):
+		firing_rates[i].append(1000*sender_fires[i]/sim_time)
 
 ###########################################
 ####   SHOW RESULTS   #####################

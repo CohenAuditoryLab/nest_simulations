8321a8c2de62cc56c042e07ab9c8b7eef9c62b31
marsalad <marsalad@sas.upenn.edu>
2017-05-31 14:07:25 -0400

    working topology model with stim and pyr layers

--- neuron_testing.py
+++ neuron_testing.py
@@ -1,4 +1,5 @@
 import nest
+import nest.topology as topp
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,27 +7,41 @@ import matplotlib.pyplot as plt
 ####   PARAMETERS   #######################
 ###########################################
 
-neuron_mod = 'iaf_psc_exp'
+freq_num = 9 # number of auditory frequencies
+amp_factor = 500 # strength of signal coming from generators
+sim_time = 500.0 # duration of simulation (ms)
+grid_size = [10.0,10.0] # side lengths of topological layers (nm)
+
+base_stim_rate = 2000.0 # stimulus rate (Hz)
+tuning_rad = 3 # broadness of tuning curve
+tono_rad = 3 # radius of stimulation for tonotopic map
+
+neuron_mod = 'iaf_psc_alpha'
 pyr_num = 12 # number of pyramidal neurons
 inh_num = 2 # number of inhibitory interneurons
 neuron_num = pyr_num + inh_num # total number of downstream neurons
 
-neuron_param = {
-	'E_L'     : -70.0, # resting membrane potential (mV)
-	'V_reset' : -70.0, # reset membrane potential after spiking (mV)
-	'V_th'    : -55.0, # spike threshold (mV)
-	'tau_m'   :  10.0  # membrane time constant (ms)
+tono_layer_param = {
+	'extent'   : grid_size,  # size of layer (nm^2)
+	'rows'     : amp_factor, # strength of signal amplification
+	'columns'  : freq_num,   # one column per frequency
+	'elements' : 'poisson_generator'
 }
-tono_weight = 500.0 # weight of stimulus connection to downstream
-pyr_weight = 200.0 # weight of excitatory synapses
-inh_weight = -50.0 # weight of inhibitory synapses
+pyr_layer_param = {
+	'extent'   : grid_size, # size of layer (nm^2)
+	'rows'     : 250,       # relative number of neurons per frequency
+	'columns'  : freq_num,  # one column per frequency
+	'elements' : neuron_mod
+}
+inh_layer_param = {
+	'extent'   : grid_size, # size of layer (nm^2)
+	'rows'     : 10,        # relative number of neurons per frequency
+	'columns'  : freq_num,  # one column per frequency
+	'elements' : neuron_mod
+}
 
-base_stim_rate = 2000.0 # stimulus rate (Hz)
-tuning_rad = 3.0 # broadness of tuning curve
-tono_rad = 3 # radius of stimulation for tonotopic map
-
-freq_num = 9 # number of auditory frequencies
-sim_time = 500.0 # duration of simulation (ms)
+pyr_weight = 1.0 # weight of excitatory synapses
+inh_weight = -1.0 # weight of inhibitory synapses
 
 # Convert raw integers to corresponding frequencies
 def freq_convert(x):
@@ -41,25 +56,38 @@ nest.ResetKernel() # reset NEST
 ####   NETWORK SETUP   ####################
 ###########################################
 
-# Tonotopic map of all simulated frequencies
-tono_map = nest.Create('poisson_generator',freq_num)
+# Create layers
+stim_layer = topp.CreateLayer(tono_layer_param)
+pyr_layer_1 = topp.CreateLayer(pyr_layer_param)
+inh_layer = topp.CreateLayer(inh_layer_param)
+pyr_layer_2 = topp.CreateLayer(pyr_layer_param)
 
-# Connect tonotopic map to downstream neurons
-pyr_neurons = nest.Create(neuron_mod, pyr_num, neuron_param)
+# Connect layers
+topp.ConnectLayers(stim_layer, pyr_layer_1, {
+	'connection_type': 'divergent',
+	'mask': {'circular': {'radius': 4.0}}, 
+	'kernel': {'gaussian': {'p_center': 1.0, 'sigma': 1.0}}})
+#topp.ConnectLayers(pyr_layer_1, pyr_layer_2, {'connection_type': 'divergent'})
+#topp.ConnectLayers(pyr_layer_1, inh_layer, {})
+#topp.ConnectLayers(inh_layer, pyr_layer_1, {})
+#topp.ConnectLayers(inh_layer, pyr_layer_2, {})
+
+'''
+# Connect layers
 for i in range(freq_num):
 	nest.Connect(tono_map[i:i+1],pyr_neurons[i:i+tono_rad], 
 		         syn_spec={'weight': tono_weight})
 
-inh_neurons = nest.Create(neuron_mod, inh_num, neuron_param)
 nest.Connect(pyr_neurons, inh_neurons, 
 	         {'rule': 'fixed_indegree', 'indegree': 4} ,
 	         syn_spec={'weight': pyr_weight})
 nest.Connect(inh_neurons, pyr_neurons, syn_spec={'weight': inh_weight})
+'''
 
 # Connect spike detectors
 spk_det = nest.Create('spike_detector')
-nest.Connect(pyr_neurons, spk_det)
-nest.Connect(inh_neurons, spk_det)
+nest.Connect(nest.GetNodes(pyr_layer_1)[0][::200], spk_det)
+
 
 ###########################################
 ####   SIMULATE   #########################
@@ -69,14 +97,13 @@ for freq in range(freq_num):
 	nest.ResetNetwork()
 	nest.SetKernelStatus({'time': 0.0})
 	
-	# Set rate for each tonotopic neuron based on frequency gof stimulus
-	for neuron in tono_map:
-		diff = abs(freq-neuron)
-		if diff < tuning_rad:
-			rate_factor = (tuning_rad-diff)/tuning_rad
-			nest.SetStatus([neuron],{'rate': rate_factor*base_stim_rate})
-		else:
-			nest.SetStatus([neuron],{'rate': 0.0})
+	# Set rate for stim_layer neurons based on frequency of stimulus
+	for row in range(amp_factor):
+		for col in range(max(0,freq-tuning_rad),
+			             min(freq_num,freq+tuning_rad+1)):
+			rate_factor = max(0.0,(tuning_rad-abs(freq-col))/float(tuning_rad))
+			nest.SetStatus(topp.GetElement(stim_layer,[col,row]),
+				           {'rate': rate_factor*base_stim_rate})
 	
 	# Simulate and record event data from spike detector
 	nest.Simulate(sim_time)
@@ -87,20 +114,26 @@ for freq in range(freq_num):
 	plt.title("Spike Trains at %s Hz" % freq_convert(freq))
 	plt.xlabel('time (ms)')
 	plt.ylabel('neuron ID')
-	plt.gca().set_ylim(1, neuron_num)
-	plt.plot(evs['times'], evs['senders']-min(pyr_neurons)+1, 'o')
-
+	plt.plot(evs['times'], evs['senders'], 'o')
+	'''
 	# Store data on firing rates
 	sender_fires = [0] * neuron_num
 	for neuron_id in evs['senders']:
 		sender_fires[neuron_id-min(pyr_neurons)] += 1
 	for i in range(neuron_num):
 		firing_rates[i].append(1000*sender_fires[i]/sim_time)
+	'''
+	# Reset rates for stim_layer neurons
+	for row in range(amp_factor):
+		for col in range(max(0,freq-tuning_rad),
+			             min(freq_num,freq+tuning_rad+1)):
+			nest.SetStatus(topp.GetElement(stim_layer,[col,row]),
+				           {'rate': 0.0})
 
 ###########################################
 ####   SHOW RESULTS   #####################
 ###########################################
-
+'''
 plt.subplots_adjust(wspace=0.3,hspace=0.6)
 plt.gcf().set_size_inches(12,10,forward=True)
 plt.figure(2)
@@ -113,4 +146,5 @@ for i in range(neuron_num):
 	y_axis = firing_rates[i]
 	plt.plot(x_axis,y_axis, label = 'Neuron %s' % str(i+1))
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.00), ncol=3)
+'''
 plt.show()

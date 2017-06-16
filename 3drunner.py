ca96e3aad1ad941b1eff29d71ceb004dde140188
marsalad <marsalad@sas.upenn.edu>
2017-06-16 13:29:54 -0400

    implemented 3d plots and added option to show/save graphs

--- 3drunner.py
+++ 3drunner.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import numpy as np
 import scipy.stats as stats
+from mpl_toolkits.mplot3d import axes3d
 from itertools import combinations
 from time import strftime
 from runtime import Runtime
@@ -11,15 +12,13 @@ from runtime import Runtime
 ####   PARAMETERS   #######################
 ###########################################
 
-# Send an email when code has run to completion?
-send_msg = True
-
-# Display all graphs to screen?
-show_graphs = False
+save_graphs = True # Autosave graphs upon creation?
+show_graphs = True # Display all graphs to screen?
+send_msg    = True # Send an email when code has run to completion?
 
 # Reference neuron_testing.py for more information on arguments
 args = {
-	'freq_num': 5,
+	'freq_num': 20,
 	'amp_factor': 100,
 	'sim_time': 100.0,
 	'grid_size': [5.0,5.0],
@@ -56,10 +55,10 @@ args = {
 
 # Specify independent variables and corresponding test ranges
 var_dict = {
-	'pyr_conn_p_center': [0.1*(i+1) for i in range(5)],
-	'pyr_conn_p_sigma': [0.1*(i+1) for i in range(5)],
-	'pyr_conn_weight_center': [0.1*(i+1) for i in range(5)],
-	#'pyr_conn_weight_sigma': [0.1*(i+1) for i in range(5)]
+	'pyr_conn_p_center': [0.1*(i+1) for i in range(10)],
+	'pyr_conn_p_sigma': [0.1*(i+1) for i in range(20)],
+	'pyr_conn_weight_center': [0.1*(i+1) for i in range(20)],
+	'pyr_conn_weight_sigma': [0.1*(i+1) for i in range(20)]
 }
 rt = Runtime(var_dict,args['freq_num'],'double')
 
@@ -76,7 +75,6 @@ for var_pair in combinations(var_dict.keys(),2):
 
 	for x in test_ranges[0]:
 		for y in test_ranges[1]:
-			curr_trial = len(rt.completed_vars)
 
 			###########################################
 			####   SIMULATION   #######################
@@ -84,9 +82,9 @@ for var_pair in combinations(var_dict.keys(),2):
 
 			args[var_pair[0]] = x
 			args[var_pair[1]] = y
-			var_num = test_ranges[0].index(x) * len(test_ranges[1]) \
+			sim_num = test_ranges[0].index(x) * len(test_ranges[1]) \
 			          + test_ranges[1].index(y)
-			firing_rates = test.main(args, rt, var_num, var_pair)
+			firing_rates = test.main(args, rt, sim_num, var_pair)
 			
 			###########################################
 			####   CALCULATIONS   #####################
@@ -95,39 +93,44 @@ for var_pair in combinations(var_dict.keys(),2):
 			# Calculate tuning curve widths and error bars
 			for n in firing_rates.keys():
 				tuning_widths = [len(j)-j.count(0.0) for j in firing_rates[n]]
-				z_vals_all[curr_trial][n] = np.mean(tuning_widths)
-				sem_vals_all[curr_trial][n] = stats.sem(tuning_widths)
+				z_vals_all[sim_num][n] = np.mean(tuning_widths)
+				sem_vals_all[sim_num][n] = stats.sem(tuning_widths)
 
 			x_vals.append(x)
 			y_vals.append(y)
 	
-	rt.inc_var(var_pair)
-
 	###########################################
 	####   GRAPHS   ###########################
 	###########################################
 
 	# Basic plot setup
-	plt.figure(len(rt.completed_vars)+1)
-	rt.inc_var(variable)
-	plt.title("Tuning Curve Width v %s" % variable)
-	plt.xlabel(variable)
-	plt.ylabel('tuning curve width')
+	fig = plt.figure(len(rt.completed_vars)+1)
+	ax = fig.gca(projection='3d')
+	plt.title("Tuning Curve Width v %s, %s" % (var_pair[0],var_pair[1]))
+	ax.set_xlabel(var_pair[0])
+	ax.set_ylabel(var_pair[1])
+	ax.set_zlabel('tuning curve width')
 
 	# Plot separate tuning curve data for pyramidal and inhibitory neurons
-	for n in firing_rates[0].keys():
+	for n in z_vals_all[0].keys():
 		if n == 'pyr':
+			c = 'blue'
 			plt_sty = 'b-'
 		else:
+			c = 'red'
 			plt_sty = 'r-'
-		y_axis = [dic[n] for dic in tun_curve_w]
-		plt.plot(test_range, y_axis, plt_sty)
-		plt.errorbar(test_range, y_axis, 
-			         yerr=[dic[n] for dic in tun_curve_sem], fmt=plt_sty)
+		z_vals = [dic[n] for dic in z_vals_all]
+		ax.plot_trisurf(x_vals, y_vals, z_vals, color=c)
 
-	# Reset variable to default value
-	args[variable] = args_default
-	plt.savefig('figures/%s %s.png' % (variable,strftime("%Y-%m-%d %H:%M")))
+	# Reset variable to default value and increment
+	args[var_pair[0]] = args_defaults[0]
+	args[var_pair[1]] = args_defaults[1]
+	rt.inc_var(var_pair)
+
+	# Save the figure
+	if save_graphs:
+		plt.savefig('figures/%s_%s %s.png' \
+			        % (var_pair[0], var_pair[1], strftime("%Y-%m-%d %H:%M")))
 
 # Display results of all simulations
 rt.final(send_msg)
